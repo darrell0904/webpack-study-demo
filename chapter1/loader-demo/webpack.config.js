76c0e209906460185cb68a57cc591f7e42b559fd
傅佳威 <fujiawei@nears.cn>
2020-01-12 23:03:04 +0800

    样式loader

--- chapter1/loader-demo/webpack.config.js
+++ chapter1/loader-demo/webpack.config.js
@@ -5,10 +5,33 @@ module.exports = {
 	entry: './src/index.js',
 	module: {
 		rules: [{
-			test: /\.png$/,
+			test: /\.(png|jpg|gif)$/,
+			use: {
+				loader: 'file-loader',
+				options: {
+					name: '[name]_[hash].[ext]',
+					outputPath: 'images/',
+				}
+			}
+		}, {
+			test: /\.(eot|ttf|svg|woff|woff2)$/,
 			use: {
 				loader: 'file-loader',
 			}
+		}, {
+			test: /\.less$/,
+			use: [
+				'style-loader',
+				{
+					loader: 'css-loader',
+					options: {
+						importLoaders: 2,
+						// modules: true,
+					}
+				},
+				'less-loader',
+				'postcss-loader',
+			]
 		}]
 	},
 	output: {
