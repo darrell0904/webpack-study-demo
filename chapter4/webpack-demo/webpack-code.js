6450375b7b6c2966f844dfdc4fcad76d478409cd
傅佳威 <fujiawei0904@163.com>
2020-02-16 22:57:55 +0800

    1，底层原理提交

--- chapter4/webpack-demo/webpack-code.js
+++ chapter4/webpack-demo/webpack-code.js
@@ -2,7 +2,10 @@
 
 "use strict"
 
-var _message = _interopRequireDefault(require("./message.js"));
+var _message = _interopRequireDefault(
+  require("./message.js")
+);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 console.log(_message["default"]);
 
@@ -24,3 +27,14 @@ exports["default"] = _default;
 
 
 // word.js code
+"use strict"
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+exports.word = void 0;
+
+var word = 'hello';
+
+exports.word = word;
