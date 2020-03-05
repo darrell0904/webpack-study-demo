fcc713e9cfdcae83bddf42c2d4befdac4ac7c1f6
傅佳威 <fujiawei0904@163.com>
2020-03-05 22:27:14 +0800

    1，ci 代码测试

--- chapter7/webpack-setting-demo/test/unit/webpack-common-test.js
+++ chapter7/webpack-setting-demo/test/unit/webpack-common-test.js
@@ -5,6 +5,6 @@ describe('webpack.common.js test case', () => {
     const baseConfig = require('../../lib/webpack.common.js')
 
     it('entry', () => {
-        assert.equal(baseConfig.entry.index, '/Users/fujiawei/Desktop/github/webpack-study-demo/chapter7/webpack-setting-demo/test/smoke/template/src/index/index.js');
+        assert.equal(baseConfig.entry.index.indexOf('webpack-study-demo/chapter7/webpack-setting-demo/test/smoke/template/src/index/index.js') > -1, true);
     });
 });
